ef8efc6ff87415b50d1f1fc132f788fbe7618a31
srph <seraphipod@gmail.com>
2016-06-28 00:20:05 +0800

    Update window settings

--- main/main.js
+++ main/main.js
@@ -4,7 +4,14 @@ import {app, BrowserWindow} from 'electron';
 let win = null;
 
 app.on('ready', () => {
-  win = new BrowserWindow({ width: 380, height: 450 });
+  win = new BrowserWindow({
+    width: 380,
+    height: 450,
+    resizable: process.env.NODE_ENV !== 'production' && process.env.WIN_RESIZABLE,
+    fullscreenable: false,
+    titleBarStyle: 'hidden'
+  });
+
   win.loadURL(`file://${__dirname}/../index.html`);
   win.webContents.openDevTools();
 
